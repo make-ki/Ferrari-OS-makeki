be6031b7e55629b3f1515a0d619e22d06f3ae31d
manvith12 <manvith270706@gmail.com>
2025-05-05 20:37:51 +0530

    added pit , a package wrapper for pacman , works on arch live now

---
+++ configs/airootfs/etc/profile.d/pit-aliases.sh
@@ -0,0 +1,8 @@
+# Ferrari OS package manager aliases
+alias update='pit -Syu'
+alias install='pit -S'
+alias remove='pit -R'
+alias search='pit -Ss'
+alias clean='pit -Sc'
+alias info='pit -Si'
+alias packages='pit -Q'
