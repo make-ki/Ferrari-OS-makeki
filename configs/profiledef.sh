d338716f9d8d7bce7f890e72f0ba330a0cfd0425
manvith12 <manvith270706@gmail.com>
2025-05-07 22:45:24 +0530

    no compression

--- configs/profiledef.sh
+++ configs/profiledef.sh
@@ -14,8 +14,8 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+airootfs_image_tool_options=('-noI' '-noD' '-noF')
+bootstrap_tarball_compression=('cat')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 
